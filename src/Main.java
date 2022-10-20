317ce55dae13a9a116e240584792bfec94597362
vgkavaldzhiev18 <vgkavaldzhiev18@codingburgas.bg>
2022-10-20 13:50:46 +0300

    Slide 8

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    String languages;
+    Main(String lang){
+        languages=lang;
+        System.out.println(languages+" Programming Language");
+    }
+
+    public static void main(String[] args) {
+        Main obj1=new Main("Java");
+        Main obj2=new Main("Python");
+        Main obj3=new Main("C++");
+    }
+
+}
